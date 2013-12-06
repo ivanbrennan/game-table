610b41953eac3d87a1051005ee9bfcd49cf77819
ahimmelstoss <ahimmelstoss@gmail.com>
2013-12-06 12:09:55 -0500

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+GameTable::Application.config.secret_key_base = '5eef2e7af5db28d315ae2cdebf88704392e41f6aba574ea31505e603f4fe84e06dcb6966fa2e47c92c59a2c8f11303032c20e8ab0f1eca7a3c22f14a199b877f'
