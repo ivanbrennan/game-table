3ea458685b8b12a31f5465f48bf07c90039ca58a
ahimmelstoss <ahimmelstoss@gmail.com>
2013-12-09 13:19:29 -0500

    great token, board, and game models and established relationships among them; migrated DB

---
+++ db/migrate/20131209180657_create_tokens.rb
@@ -0,0 +1,12 @@
+class CreateTokens < ActiveRecord::Migration
+  def change
+    create_table :tokens do |t|
+      t.string :type
+      t.integer :x_coordinate
+      t.integer :y_coordinate
+      t.string :image
+      t.integer :game_id, :index => true
+      t.timestamps
+    end
+  end
+end
