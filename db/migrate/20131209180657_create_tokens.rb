f2cd043dc96e8d05dc41f2355f33775bdfa93e15
J-Scag <j_scag@yahoo.com>
2013-12-09 19:42:09 -0500

    Add seed file

--- db/migrate/20131209180657_create_tokens.rb
+++ db/migrate/20131209180657_create_tokens.rb
@@ -1,7 +1,7 @@
 class CreateTokens < ActiveRecord::Migration
   def change
     create_table :tokens do |t|
-      t.string :type
+      t.string :name
       t.integer :x_coordinate
       t.integer :y_coordinate
       t.string :image
