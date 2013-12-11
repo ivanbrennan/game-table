49d3d87a15b7bd0f97a3f344bd5caf5541fa97a0
ivanbrennan <ivan.brennan@gmail.com>
2013-12-10 19:19:12 -0500

    messages route

---
+++ app/controllers/messages_controller.rb
@@ -0,0 +1,21 @@
+class MessagesController < ApplicationController
+  #before_action :set_token, only: [:move]
+  #enable_sync only: [:move]
+
+
+#  def move
+#    @token.update(:x_coordinate => params[:x_coordinate].to_i, :y_coordinate => params[:y_coordinate].to_i)
+#    sync_update @token
+#    redirect_to @token.game
+#  end
+
+  def create
+  end
+
+  private
+    # Use callbacks to share common setup or constraints between actions.
+  def set_token
+    @token = Token.find(params[:id])
+  end
+
+end
