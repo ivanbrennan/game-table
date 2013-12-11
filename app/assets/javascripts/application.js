babbac5021539cb4d187b299db6c29cbfcc9de43
J-Scag <j_scag@yahoo.com>
2013-12-10 22:14:29 -0500

    Working on styling game show page

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -18,13 +18,15 @@
 
 $(window).load(function(){
 
-  $(".token").draggable({});
+  $(".board").mousedown(function(){
+    return false;
+  });
+
+  $(".token").draggable({ containment: "parent" });
 
   $(".token").on("dragstop", function(event, ui){
-    var x = $(this).offset().left;
-    var y = $(this).offset().top;
-    x > 0 ? x : x = 0;
-    y > 0 ? y : y = 0;
+    var x = $(this).position().left;
+    var y = $(this).position().top;
     var tokenId = $(this).data("id");
     var valuesToSubmit = "x_coordinate=" + x + "&y_coordinate="+ y
     $.post("tokens/"+tokenId+"/move", valuesToSubmit, function(){})
